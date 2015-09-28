9f324ece3e3cc6a651c334f5f5fad92d72d8824d
drammock <dan.mccloy@gmail.com>
2015-09-28 12:05:38 -0700

    add kernel trace to plots

--- figures/fig-1.py
+++ figures/fig-1.py
@@ -15,17 +15,15 @@ pupil diameter for target and non-target tones.
 import numpy as np
 import os.path as op
 import matplotlib.pyplot as plt
+from pyeparse.utils import pupil_kernel
+from convenience_functions import box_off
 plt.ioff()
 
-
-def box_off(ax):
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-    ax.get_xaxis().set_tick_params(which='both', direction='out')
-    ax.get_yaxis().set_tick_params(which='both', direction='out')
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
-
+# flags
+plot_kernel = True
+savefig = True
+fs = 1000
+kernel_dur = 3.
 
 # file I/O
 work_dir = '..'
@@ -40,15 +38,22 @@ across_subj_sem = np.std(avg_data, axis=0) / np.sqrt(len(avg_data) - 1)
 maxs = across_subj.max(axis=-1)
 idxs = across_subj.argmax(axis=-1)
 for ii, (dat, sem) in enumerate(zip(across_subj, across_subj_sem)):
+    t_max = t[idxs[ii]]
     lab = ['a)', 'b)'][ii]
     labx = [-0.3, -0.15][ii]
-    _ = axs[ii].fill_between(t, dat-sem, dat+sem, color='0.7')
-    _ = axs[ii].plot(t, dat, color='k', linewidth=1)
-    _ = axs[ii].annotate('t = ' + str(round(t[idxs[ii]], 3)),
-                         xy=(t[idxs[ii]], maxs[ii]), xytext=(0, 12),
-                         textcoords='offset points', ha='center')
-    _ = axs[ii].axhline(0, linestyle=':', color='0.3')
-    _ = axs[ii].axvline(0, linestyle=':', color='0.3')
+    _ = axs[ii].fill_between(t, dat-sem, dat+sem, color='0.7', zorder=2)
+    _ = axs[ii].plot(t, dat, color='w', linewidth=0.75, zorder=2)
+    if plot_kernel:
+        kernel = pupil_kernel(fs, t_max=t_max, dur=kernel_dur)
+        kernel = kernel * (maxs[ii] / kernel[np.argmax(kernel)])  # scale
+        t_kernel = np.linspace(0, kernel_dur, kernel_dur * fs)
+        _ = axs[ii].plot(t_kernel, kernel, color='k', linestyle=':',
+                         linewidth=1.25, zorder=3)
+    _ = axs[ii].annotate('t = ' + str(round(t_max, 3)), xy=(t_max, maxs[ii]),
+                         xytext=(0, 12), textcoords='offset points',
+                         ha='center')
+    _ = axs[ii].axhline(0, color='0.8', linewidth=0.5, zorder=1)
+    _ = axs[ii].axvline(0, color='0.8', linewidth=0.5, zorder=1)
     _ = axs[ii].get_xaxis().set_label_text('time (s)')
     _ = axs[ii].text(labx, 1, lab, transform=axs[ii].transAxes,
                      fontdict=dict(weight='bold'))
@@ -56,5 +61,11 @@ _ = axs[0].get_yaxis().set_label_text('Pupil size (z-score)')
 
 for ax in axs:
     box_off(ax)
+    ax.set_xlim(-0.5, 2.5)
 fig.tight_layout(h_pad=0.3)
-fig.savefig('fig-1.eps')
+
+if savefig:
+    fig.savefig('fig-1.eps')
+else:
+    plt.ion()
+    plt.show()
