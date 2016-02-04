07161d29e4d5d18abe82948454988b8c7f20700c
drammock <dan.mccloy@gmail.com>
2016-02-03 18:03:42 -0800

    fix cue letters

--- figures/fig-2.py
+++ figures/fig-2.py
@@ -48,7 +48,7 @@ centers_y = [2] * 6 + [0] * 4
 box_x = [(0, 0, 1, 1)] + [(1.5, 1.5, 2.5, 2.5), (3.1, 3.1, 4.1, 4.1)] * 2
 box_y = [(1.5, 2.5, 2.5, 1.5)] * 3 + [(-0.5, 0.5, 0.5, -0.5)] * 2
 # cue AB target O foils DEGPUV
-box_l = ['AA', 'AB', 'E', 'O', 'P', 'O', 'G', 'V', 'E', 'U']
+box_l = ['AA', 'AU', 'E', 'O', 'D', 'E', 'P', 'V', 'P', 'O']
 color = [maintcol, switchcol] + [lettercol] * 8
 bcolor = [cuecol] + [malecol] * 2 + [femalecol] * 2
 ecolor = ['k'] + ['none'] * 4
